55ee098e24cbc63dafb6385c3a06ffdb5d36d440
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-04-12 13:19:07 -0400

    Remove unnecessary sudo usage in brew.sh

--- brew.sh
+++ brew.sh
@@ -2,12 +2,6 @@
 
 # Install command-line tools using Homebrew.
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -23,7 +17,7 @@ brew bundle
 
 # Set up GNU core utilities (those that come with OS X are outdated).
 echo 'Be sure to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.'
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 if ! grep '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
